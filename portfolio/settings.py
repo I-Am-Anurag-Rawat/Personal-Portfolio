f7f0dc030cc6ac5faa0728639b3bb228959d342c
Anurag Rawat <rawatanurag180@gmail.com>
2025-06-24 19:24:15 +0530

    solved errors

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -114,18 +114,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 import os
-
-STATIC_URL = '/static/'
-
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# (Optional) only if you have static in custom places:
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),  # change 'app' to your app name
-]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
