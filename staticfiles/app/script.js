fa422812c74cc4fb9595dc171cbfa712630bbbe8
Anurag Rawat <rawatanurag180@gmail.com>
2025-07-28 04:55:40 +0530

    updated staticfiles

--- staticfiles/app/script.js
+++ staticfiles/app/script.js
@@ -5,6 +5,68 @@ const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
 const progressBarPercents = [90, 80, 85, 80, 80, 70, 70];
 
+
+
+
+const form = document.getElementById('contactForm');
+const submitBtn = form.querySelector('input[type="submit"]'); // Get the submit button
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Disable the submit button
+    submitBtn.disabled = true;
+    submitBtn.value = 'Sending...';
+    const formData = new FormData(form);
+    fetch('', {
+        method: 'POST',
+        headers: {
+          'X-CSRFToken': getCookie('csrftoken'), // send CSRF token for Django security
+        },
+        body: formData
+      })
+      .then(response => {
+        if(response.status == 204)
+        {
+          console.log('Resetting form on 204');
+          form.reset(); // clear the form
+          return null;
+        }
+        else
+        {
+          return response.json()
+        }
+      })  // get JSON response from Django
+      .then(data => {
+        if(data) {
+          form.reset(); // clear the form
+        }
+      })
+      .catch(error => {
+        console.error('Error sending form:', error);
+      })
+      .finally(() => {
+          // Re-enable the button no matter what
+          submitBtn.disabled = false;
+          submitBtn.value = 'Send'; // Restore original text
+      });
+});
+
+// Get CSRF token from cookie
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+}
+
+
 window.addEventListener("scroll", () => {
   mainFn();
 });
